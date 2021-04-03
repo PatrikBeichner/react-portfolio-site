e3a167a20f8c4a239d1c4e47049d81513488cde2
PatrikBeichner <patrik.beichner@gmail.com>
2021-04-02 21:08:22 -0400

    Menu blur progress

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,7 +12,7 @@ export default function Blog() {
             <div className='blog'>
                 <div className='text'>
                     <h1> hi this is blog</h1>
-                    <p> heres a bunch iof tesdklsjdksjhfkshfkshfksjfhahfjshfjksf</p>
+                    <p> Löksås ipsum varit är se dimmhöljd inom göras tid häst både och, när kanske upprätthållande i ännu rännil gamla sin hela. Fram genom tre verkligen erfarenheter sista tiden omfångsrik dimma hans räv ännu, varit mot brunsås omfångsrik och tre stora blivit hela dock och hans, dimma kom dimma därmed på tidigare där både år upprätthållande. För brunsås är när se söka miljoner tid ska, sista när samma del tiden där åker ordningens, plats bra tre oss och strand fram.</p>
                 </div>
             </div>
         </>
